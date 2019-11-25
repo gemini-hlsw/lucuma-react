695d29876368855d432511945747003eb0fd1424
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2019-11-25 15:11:55 -0300

    Use react props
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

---
+++ facade/src/test/scala/react/clipboard/CopyToClipboardTest.scala
@@ -0,0 +1,20 @@
+package react
+
+package clipboard
+
+import japgolly.scalajs.react.vdom.html_<^.{< => <<, _}
+import react.common.TestUtils
+import utest._
+
+object CopyToClipboardTest extends TestSuite with TestUtils {
+
+  val tests = Tests {
+    'render - {
+      val table =
+        CopyToClipboard(text = "text")(<<.div("Copy"))
+      val html =
+        """<div><div>Copy</div></div>""".stripMargin.replaceAll("[\n\r]", "")
+      assertRender(table, html)
+    }
+  }
+}
