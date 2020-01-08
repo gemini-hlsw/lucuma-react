3360be1869c2842dad29e07ae4b0f05f77805ceb
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-01-08 15:55:04 -0300

    Update scala and scalajs-react
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- test/src/test/scala/react/common/StyleTests.scala
+++ test/src/test/scala/react/common/StyleTests.scala
@@ -6,19 +6,19 @@ import utest._
 
 object StyleTests extends TestSuite with TestUtils {
   val tests = Tests {
-    'style - {
+    test("style") {
       val a: Style = Style(Map("height" -> 42, "background" -> "black"))
       val b: Style = Style(Map("height" -> 42, "background" -> "black"))
       val c: Style = Style(Map("height" -> 42, "background" -> "black"))
       assert((a |+| b) === c)
     }
-    'extract - {
+    test("extract") {
       val a: Style = Style(Map("height" -> 42, "background" -> "black"))
       assert(a.extract[Int]("height") == 42.some)
       assert(a.extract[String]("height").isEmpty)
       assert(a.extract[String]("background") == "black".some)
     }
-    'remove - {
+    test("remove") {
       val a: Style = Style(Map("height" -> 42, "background" -> "black"))
       assert(a.remove("height").extract[String]("height").isEmpty)
     }
