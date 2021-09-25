8cc425e431fb3d4ed7d4443a515b0ff195b9345a
Raúl Piaggio <rpiaggio@gemini.edu>
2021-09-24 22:13:15 -0300

    JS components: add .withRef and allow facades.

--- common/src/main/scala/react/common/package.scala
+++ common/src/main/scala/react/common/package.scala
@@ -4,8 +4,7 @@ import japgolly.scalajs.react.CtorType
 import japgolly.scalajs.react.vdom.VdomElement
 import japgolly.scalajs.react.vdom.VdomNode
 import japgolly.scalajs.react.component.Generic
-import japgolly.scalajs.react.component.Js.RawMounted
-import japgolly.scalajs.react.component.Js.UnmountedWithRawType
+import japgolly.scalajs.react.component.Js._
 import japgolly.scalajs.react.component.Scala
 import japgolly.scalajs.react.vdom.TagMod
 import react.common.syntax.AllSyntax
@@ -65,10 +64,12 @@ package object common extends AllSyntax {
   // End Scala Components
 
   // Begin JS Components
-  type RenderFn[P]             = Generic.Unmounted[P, Unit]
-  type Render[P <: js.Object]  = UnmountedWithRawType[P, Null, RawMounted[P, Null]]
-  type RenderFnC[P]            = CtorType.ChildrenArgs => RenderFn[P]
-  type RenderC[P <: js.Object] = CtorType.ChildrenArgs => Render[P]
+  type RenderFn[P]                              = Generic.Unmounted[P, Unit]
+  type Render[P <: js.Object]                   = Unmounted[P, Null]
+  type RenderF[P <: js.Object, F <: js.Object]  = UnmountedWithFacade[P, Null, F]
+  type RenderFnC[P]                             = CtorType.ChildrenArgs => RenderFn[P]
+  type RenderC[P <: js.Object]                  = CtorType.ChildrenArgs => Render[P]
+  type RenderCF[P <: js.Object, F <: js.Object] = CtorType.ChildrenArgs => RenderF[P, F]
 
   type GenericFnComponentP[P <: js.Object]      = GenericFnComponent[P, CtorType.Props, Unit]
   type GenericFnComponentPC[P <: js.Object, A]  =
@@ -83,6 +84,15 @@ package object common extends AllSyntax {
   type GenericComponentPA[P <: js.Object, A]  = GenericJsComponentA[P, CtorType.Props, Unit, A]
   type GenericComponentPAC[P <: js.Object, A] =
     GenericJsComponentAC[P, CtorType.PropsAndChildren, Unit, A]
+
+  type GenericComponentPF[P <: js.Object, F <: js.Object]      =
+    GenericJsComponentF[P, CtorType.Props, Unit, F]
+  type GenericComponentPCF[P <: js.Object, A, F <: js.Object]  =
+    GenericJsComponentCF[P, CtorType.PropsAndChildren, Unit, A, F]
+  type GenericComponentPAF[P <: js.Object, A, F <: js.Object]  =
+    GenericJsComponentAF[P, CtorType.Props, Unit, A, F]
+  type GenericComponentPACF[P <: js.Object, A, F <: js.Object] =
+    GenericJsComponentACF[P, CtorType.PropsAndChildren, Unit, A, F]
 
   implicit class GenericFnComponentPCOps[P <: js.Object, A](val c: GenericFnComponentPC[P, A])
       extends AnyVal {
