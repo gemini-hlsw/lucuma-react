2a2d525516f9544981d4fa6d2c44b5ccebe8349b
Scala Steward <me@scala-steward.org>
2021-09-25 20:45:45 +0200

    Reformat with scalafmt 3.0.5

--- build.sbt
+++ build.sbt
@@ -58,7 +58,7 @@ inThisBuild(
   )
 )
 
-val root =
+val root        =
   project
     .in(file("."))
     .aggregate(facade, demo)
@@ -72,7 +72,7 @@ val root =
       Keys.`package`  := file("")
     )
 
-lazy val demo =
+lazy val demo   =
   project
     .in(file("demo"))
     .enablePlugins(ScalaJSBundlerPlugin)
--- facade/src/main/scala/reactST/reactTable/syntax.scala
+++ facade/src/main/scala/reactST/reactTable/syntax.scala
@@ -4,7 +4,7 @@ import japgolly.scalajs.react.facade
 import reactST.reactTable.mod.Cell
 
 object syntax {
-  implicit class CellOps[Self <: Cell[_, _]](val cell: Self) extends AnyVal {
+  implicit class CellOps[Self <: Cell[_, _]](val cell: Self)             extends AnyVal {
     def renderCell: facade.React.Node = cell.render_Cell(reactTableStrings.Cell)
   }
 
