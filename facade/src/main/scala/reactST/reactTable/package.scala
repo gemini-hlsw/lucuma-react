98733f8946f283dd6c47854b5cda78ce053be619
Todd Burnside <tburnside@gemini.edu>
2021-09-28 12:20:28 -0700

    Change scalafmt config

--- facade/src/main/scala/reactST/reactTable/package.scala
+++ facade/src/main/scala/reactST/reactTable/package.scala
@@ -3,7 +3,7 @@ package reactST
 import reactST.reactTable.mod._
 
 package object reactTable extends HooksApiExt {
-  type ColumnOptions[D]                                  =
+  type ColumnOptions[D] =
     ColumnInterface[D]
       with reactST.reactTable.anon.IdIdType[D]
       with reactST.reactTable.anon.`0`[D]
@@ -11,6 +11,6 @@ package object reactTable extends HooksApiExt {
       with ColumnFooter[D]
   type ColumnValueOptions[D, V, Col <: ColumnOptions[D]] =
     Col with ColumnInterfaceBasedOnValue[D, V]
-  type ColumnGroupOptions[D]                             = ColumnGroup[D] with ColumnFooter[D]
-  type ColumnObject[D]                                   = ColumnInstance[D] with UseTableColumnFooter[D]
+  type ColumnGroupOptions[D] = ColumnGroup[D] with ColumnFooter[D]
+  type ColumnObject[D]       = ColumnInstance[D] with UseTableColumnFooter[D]
 }
