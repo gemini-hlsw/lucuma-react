a912c6f080dfc7da0f6e766ff3e8ea9f2bcc02ea
Arman Bilge <armanbilge@gmail.com>
2022-03-23 21:48:50 +0000

    Import svgdotjs

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,9 @@
 val sbtLucumaVersion = "0.6.3"
-addSbtPlugin("edu.gemini" % "sbt-lucuma-lib"         % sbtLucumaVersion)
-addSbtPlugin("edu.gemini" % "sbt-lucuma-sjs-bundler" % sbtLucumaVersion)
+addSbtPlugin("edu.gemini"                  % "sbt-lucuma-lib"         % sbtLucumaVersion)
+addSbtPlugin("edu.gemini"                  % "sbt-lucuma-sjs-bundler" % sbtLucumaVersion)
+addSbtPlugin("org.scalablytyped.converter" % "sbt-converter"          % "1.0.0-beta37+8-3ddfae51-SNAPSHOT")
+
+resolvers += MavenRepository(
+  "sonatype-s01-snapshots",
+  "https://s01.oss.sonatype.org/content/repositories/snapshots"
+)
---
+++ svgdotjs/src/main/scala/lucuma/svgdotjs/package.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma
+
+package object svgdotjs {
+
+  type ArrayXY     = svgdotjs.svgdotjsSvgJs.mod.ArrayXY
+  type Container   = svgdotjs.svgdotjsSvgJs.mod.Container
+  type PointArray  = svgdotjs.svgdotjsSvgJs.mod.PointArray
+  type SVGArray[T] = svgdotjs.svgdotjsSvgJs.mod.Array[T]
+  type Element     = svgdotjs.svgdotjsSvgJs.mod.Element
+  type Svg         = svgdotjs.svgdotjsSvgJs.mod.Svg
+  type Polygon     = svgdotjs.svgdotjsSvgJs.mod.Polygon
+  type Group       = svgdotjs.svgdotjsSvgJs.mod.G
+  type Rect        = svgdotjs.svgdotjsSvgJs.mod.Rect
+  type Matrix      = svgdotjs.svgdotjsSvgJs.mod.Matrix
+}
