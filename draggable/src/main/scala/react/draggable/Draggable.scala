a2612830ec2dea67d9dc09b47f07a4d55a1cbced
Arman Bilge <armanbilge@gmail.com>
2022-03-23 17:43:08 +0000

    WIP importing react-draggable

--- draggable/src/main/scala/react/draggable/Draggable.scala
+++ draggable/src/main/scala/react/draggable/Draggable.scala
@@ -1,9 +1,11 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package react
 
 package draggable
 
 import scala.scalajs.js
-import scala.scalajs.js.|
 import js.annotation.JSImport
 import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.Children
