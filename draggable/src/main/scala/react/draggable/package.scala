ee3041c24a5c77cd5e022abee91f7403f93aa693
Arman Bilge <armanbilge@gmail.com>
2022-03-25 14:02:43 +0000

    JsNumber -> Double

--- draggable/src/main/scala/react/draggable/package.scala
+++ draggable/src/main/scala/react/draggable/package.scala
@@ -5,7 +5,6 @@ package react
 
 import scala.scalajs.js
 import scala.scalajs.js.|
-import japgolly.scalajs.react.facade.JsNumber
 import org.scalajs.dom.html.{ Element => HTMLElement }
 import org.scalajs.dom.MouseEvent
 import react.common.EnumValue
@@ -26,20 +25,20 @@ package draggable {
     case object None extends Axis
   }
 
-  case class Grid(x: JsNumber, y: JsNumber) {
-    val value: js.Array[JsNumber] = js.Array(x, y)
+  case class Grid(x: Double, y: Double) {
+    val value: js.Array[Double] = js.Array(x, y)
   }
 
   @js.native
   trait ControlPosition extends js.Object {
-    var x: js.UndefOr[JsNumber]
-    var y: js.UndefOr[JsNumber]
+    var x: js.UndefOr[Double]
+    var y: js.UndefOr[Double]
   }
 
   object ControlPosition {
     def apply(
-      x: js.UndefOr[JsNumber] = js.undefined,
-      y: js.UndefOr[JsNumber] = js.undefined
+      x: js.UndefOr[Double] = js.undefined,
+      y: js.UndefOr[Double] = js.undefined
     ): ControlPosition = {
       val p = (new js.Object).asInstanceOf[ControlPosition]
       p.x = x
@@ -50,14 +49,14 @@ package draggable {
 
   @js.native
   trait DraggableBounds extends js.Object {
-    var left: JsNumber
-    var right: JsNumber
-    var top: JsNumber
-    var bottom: JsNumber
+    var left: Double
+    var right: Double
+    var top: Double
+    var bottom: Double
   }
 
   object DraggableBounds {
-    def apply(left: JsNumber, right: JsNumber, top: JsNumber, bottom: JsNumber): DraggableBounds = {
+    def apply(left: Double, right: Double, top: Double, bottom: Double): DraggableBounds = {
       val p = (new js.Object).asInstanceOf[DraggableBounds]
       p.left = left
       p.right = right
@@ -70,24 +69,24 @@ package draggable {
   @js.native
   trait DraggableData extends js.Object {
     var node: HTMLElement = js.native
-    var x: JsNumber       = js.native
-    var y: JsNumber       = js.native
-    var deltaX: JsNumber  = js.native
-    var deltaY: JsNumber  = js.native
-    var lastX: JsNumber   = js.native
-    var lastY: JsNumber   = js.native
+    var x: Double       = js.native
+    var y: Double       = js.native
+    var deltaX: Double  = js.native
+    var deltaY: Double  = js.native
+    var lastX: Double   = js.native
+    var lastY: Double   = js.native
   }
 
   @js.native
   trait PositionOffsetControlPosition extends js.Object {
-    var x: js.UndefOr[String | JsNumber]
-    var y: js.UndefOr[String | JsNumber]
+    var x: js.UndefOr[String | Double]
+    var y: js.UndefOr[String | Double]
   }
 
   object PositionOffsetControlPosition {
     def apply(
-      x: js.UndefOr[String | JsNumber] = js.undefined,
-      y: js.UndefOr[String | JsNumber] = js.undefined
+      x: js.UndefOr[String | Double] = js.undefined,
+      y: js.UndefOr[String | Double] = js.undefined
     ): PositionOffsetControlPosition = {
       val p = (new js.Object).asInstanceOf[PositionOffsetControlPosition]
       p.x = x
