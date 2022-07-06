7a34f5362e09b5f910815e4d5921d2de33fa4edb
Arman Bilge <armanbilge@gmail.com>
2022-07-06 17:31:25 +0000

    formatting

--- grid-layout-demo/src/main/scala/react/gridlayout/Demo.scala
+++ grid-layout-demo/src/main/scala/react/gridlayout/Demo.scala
@@ -38,7 +38,7 @@ object RGLDemo {
         )
 
       println(
-        getBreakpointFromWidth(layouts.map { case (x, (w, _, _)) => (x -> w) },
+        getBreakpointFromWidth(layouts.map { case (x, (w, _, _)) => x -> w },
                                useResize.width.orEmpty
         )
       )
